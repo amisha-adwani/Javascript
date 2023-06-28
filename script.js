0b55bc828aee0a16181d0e99ab500425165bada7
amisha <amishaadwani0023@gmail.com>
2023-06-28 19:31:31 +0530

    new changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const num = Math.floor(Math.random() * 100 + 1);
+let output = document.getElementById("output");
 console.log("random number = " + num);
 document.getElementById("myButton").onclick = function () {
   checkNum();
@@ -6,37 +7,26 @@ document.getElementById("myButton").onclick = function () {
 
 function checkNum() {
   let chance = 0;
-  for (let i = chance; i < 3; i++) {
+  for (chance; chance < 3; chance++) {
     let userGuess = document.getElementById("userNum").value;
-
-    //if (chance < 3) {
-      if (userGuess === "") {
-        console.log("Invalid number");
-      } else if (userGuess == 0) {
-        console.log("The number is between 1 to 100");
-      }  else if(chance===3) {
-        console.log("Chances over");
+    if (userGuess === "") {
+      output.innerHTML = `Invalid number`;
+    } else if (userGuess == 0) {
+      output.innerHTML = `The number is between 1 to 100`;
+    } else {
+      switch (true) {
+        case num == userGuess:
+          output.innerHTML = "Great, right guess";
+          break;
+        case userGuess < num:
+          output.innerHTML = `Guess a larger number`;
+          break;
+        case userGuess > num:
+          output.innerHTML = `Guess a smaller number`;
+          break;
       }
-      else {
-        switch (true) {
-          case num == userGuess:
-            console.log("Great, right guess");
-            break;
-          case userGuess < num:
-            console.log("Guess a larger number");
-            chance++;
-            break;
-          case userGuess > num:
-            console.log("Guess a smaller number");
-           chance++;
-            break;
-        }
-      }
-      //chance++;
-  //  } 
-//   else {
-//       console.log("Chances over");
-//     }
+    }
   }
+  
 }
 
